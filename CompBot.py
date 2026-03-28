ba5e003d62ce46fba77835b0788035d72f2de6c5
Kyle <128479271+Oaketree07@users.noreply.github.com>
2026-03-28 12:13:39 +0000

    CompBot so far :D
    Implement basic Discord bot with member join event and ping command.

---
+++ CompBot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True  # IMPORTANT
+intents.members = True  # Required for member join events
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}") # Test command
+
+@bot.event
+async def on_member_join(member):
+    await member.send("Welcome to the CompSoc Discord Server!") # Someone make a better message than this, I am not creative
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong 🏓")
+
+bot.run("MTQ4NzQwNjg2OTcwNTI2MTEyNg.GfH12M.ASk3HsbAk_YiUBBoZ9xsBCJtCHreDCVdzn4GRM")
