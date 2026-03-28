526925ce2debf6ac6b1f34bb6ece5136e32fb82e
Kyle <128479271+Oaketree07@users.noreply.github.com>
2026-03-28 13:37:53 +0000

    Enhance member welcome and add quiz command
    Added a quiz command and improved welcome messages for new members.

--- CompBot.py
+++ CompBot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import requests
+import io
 
 intents = discord.Intents.default()
 intents.message_content = True  # IMPORTANT
@@ -9,14 +11,20 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}") # Test command
+    print(f"Logged in as {bot.user}")
 
 @bot.event
 async def on_member_join(member):
     await member.send("Welcome to the CompSoc Discord Server!") # Someone make a better message than this, I am not creative
+    await member.send("To get started take this short quiz to find out which Sig you belong to")
+    await member.send("https://societyq.netlify.app")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong 🏓")
+
+@bot.command()
+async def quiz(ctx):
+    await ctx.send("Take the quiz to find out which Sig you belong to! https://societyq.netlify.app")
 
 bot.run("No token for u")
